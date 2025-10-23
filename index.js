b7fc3a4e899155fa977cad77c186b40fded8199a
AbdelrahmanMohamed-sa3oor <abdelrahmaniphone2@gmail.com>
2025-10-23 17:41:36 +0300

    addp

--- index.js
+++ index.js
@@ -40,7 +40,10 @@ app.use("/api/opponents", opponentRoutes);
 
 // DB connection
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI,{
+      useNewUrlParser: true,
+  useUnifiedTopology: true,
+  })
   .then(() => console.log("✅ Connected to MongoDB"))
   .catch((err) => console.error("❌ DB Error:", err));
 
