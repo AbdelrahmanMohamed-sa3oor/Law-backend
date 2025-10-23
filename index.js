4f438564804da0ccc06b0c517f2d8c358d143207
AbdelrahmanMohamed-sa3oor <abdelrahmaniphone2@gmail.com>
2025-10-23 17:48:22 +0300

    Fix CORS and add build script

--- index.js
+++ index.js
@@ -21,6 +21,15 @@ const app = express();
 
 // Middleware
 app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 app.use(express.json({ limit: "10mb" }));
 
 // Test route
